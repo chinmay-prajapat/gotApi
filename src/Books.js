a4ad33df7494945d4e1b6e72080e69de5c2dda96
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-06 20:18:43 +0530

    fetch data

--- src/Books.js
+++ src/Books.js
@@ -1,18 +1,78 @@
 import got from "./apis/apis";
 import React, { useEffect, useState } from "react";
 const Books = () => {
-  const [data, setData] = useState();
+  const [bookData, setBookData] = useState();
   useEffect(() => {
     (async () => {
-      const res = await got.get("/books/");
-      setData(res);
+      const { data } = await got.get("/books/");
+      setBookData(data);
     })();
   }, []);
-  console.log(data);
+  console.log(bookData);
   return (
-    <div>
-      <h1>WelCome To Got Family</h1>
-      {!data ? <p>Loading...</p> : <p>Data</p>}
+    <div className="ui container">
+      <div className="ui segment" style={{ marginTop: "10px" }}>
+        <h1 style={{ textAlign: "center" }}>WelCome To Got World</h1>
+      </div>
+      {!bookData ? (
+        <div className="ui active dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+      ) : (
+        <div
+          className="ui relaxed divided list"
+          style={{ margin: "30px 0 30px 0" }}
+        >
+          {bookData.map((book, index) => {
+            const {
+              name,
+              authors,
+              country,
+              numberOfPages,
+              publisher,
+              released,
+            } = book;
+            return (
+              <div className="item" key={index}>
+                <div className="content">
+                  <p className="header" style={{ marginTop: "10px" }}>
+                    <i className="fas fa-book"></i>&nbsp;{name}
+                  </p>
+
+                  <div class="ui list">
+                    <div className="item">
+                      <div class="content">
+                        <b>Total Pages:&nbsp;</b>
+                        {numberOfPages}
+                      </div>
+                      <div class="content">
+                        <b>Publisher:&nbsp;</b>
+                        {publisher}
+                      </div>
+
+                      <div class="content">
+                        <b>Country:&nbsp;</b>
+                        {country}
+                      </div>
+                      <div class="content">
+                        <b>Released:&nbsp;</b>
+                        {released.slice(0, 10)}
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="description"
+                    style={{ textAlign: "right", marginBottom: "10px" }}
+                  >
+                    {authors[0]}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <hr style={{ padding: "10px" }} />
     </div>
   );
 };
